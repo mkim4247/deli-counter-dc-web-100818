e34fc9c841d3dc410f802663233a5aa1ca07b9ce
Michael Kim <m.kim4247@gmail.com>
2020-10-07 19:03:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ end
 
 def take_a_number(current_line, new_person_name)
   current_line.push(new_person_name)
-  puts "Welcome, #{name}. You are number #{current_line.index(name)+1} in line."
+  puts "Welcome, #{new_person_name}. You are number #{current_line.index(new_person_name)+1} in line."
   end
 end
 
