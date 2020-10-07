b07b362fd7368c147d3f24ae95fe0293c4204dad
Michael Kim <m.kim4247@gmail.com>
2020-10-07 19:08:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,7 +22,7 @@ end
 def now_serving(line_array)
   if line_array > 0
     puts "Currently serving #{line_array.first}."
-    
+    line_array.shift
   else
     puts "The line is currently empty."
   
