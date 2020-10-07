a1236a03bff6c309d72a763e9f9f10778d800359
Michael Kim <m.kim4247@gmail.com>
2020-10-07 19:05:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,3 +19,6 @@ def take_a_number(current_line, new_person_name)
   puts "Welcome, #{new_person_name}. You are number #{current_line.index(new_person_name)+1} in line."
 end
 
+def now_serving(line_array)
+  puts "Currently serving #{}"
+end
